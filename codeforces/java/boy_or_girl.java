0e414a35aec586174c99382551afe3795a89a702
Saagar Deshpande <raysaagar@gmail.com>
2013-01-21 23:10:54 -0500

    codeforces 158A

---
+++ codeforces/java/boy_or_girl.java
@@ -0,0 +1,28 @@
+/*********************************
+ * Codeforces 158A - Boy or Girl
+ *********************************/
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class boy_or_girl {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		HashSet<String> letters = new HashSet<String>(100);
+
+		String user = scan.nextLine();
+		
+		for(int i = 0; i < user.length(); i++){
+			letters.add(user.substring(i, i+1));
+		}
+		
+		if(letters.size() % 2 == 0){
+			System.out.println("CHAT WITH HER!");
+		}
+		else{
+			System.out.println("IGNORE HIM!");
+		}
+		
+	}
+
+}
