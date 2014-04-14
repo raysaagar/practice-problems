54c87911e38da1eb8efed781f04a33357ac671dd
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-14 12:42:26 -0400

    spacing lul

--- codeforces/java/boy_or_girl.java
+++ codeforces/java/boy_or_girl.java
@@ -11,18 +11,18 @@ public class boy_or_girl {
 		HashSet<String> letters = new HashSet<String>(100);
 
 		String user = scan.nextLine();
-		
+
 		for(int i = 0; i < user.length(); i++){
 			letters.add(user.substring(i, i+1));
 		}
-		
+
 		if(letters.size() % 2 == 0){
 			System.out.println("CHAT WITH HER!");
 		}
 		else{
 			System.out.println("IGNORE HIM!");
 		}
-		
+
 	}
 
 }
